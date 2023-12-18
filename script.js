8d7e7f2557a4fafdd46c7aabcb3656f77346c7e4
prajwal2103 <137304916+prajwal2103@users.noreply.github.com>
2023-12-18 21:21:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function firstWord(s) {
   // your code here
+	if(s.length==0){
+		return '';
+	}
+	s=s.trim();
+    let word=s.split(' ');
+	return word[0];
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
-alert(firstWord(s));
+alert(firstWord(s))
